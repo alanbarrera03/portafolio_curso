1eeaa0662564930043468d961eaa69b52b392296
Alan Barrera <aalan.eduardo@gmail.com>
2020-04-20 23:57:49 -0500

    Fin sección 4

---
+++ src/app/interfaces/info-pagina.interfaces.ts
@@ -0,0 +1,10 @@
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  nombe_corto?: string;
+  pagina_autor?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  equipo_trabajo?: any[];
+}
